7c4d00bae8d82948c2d38d4a0d41f03e056b37cc
ethan-ngo <ethanngo3539@gmail.com>
2025-10-25 21:39:01 -0700

    added queue page

--- nurse-ui/src/App.jsx
+++ nurse-ui/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import InputLoginPage from './pages/input_test'
 import NurseForm from './pages/nurse_form'
+import Queue from './pages/Queue'
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<p>Hello World</p>} />
+            <Route path="/" element={<Queue />} />
             <Route path="/inputtest" element={<InputLoginPage />} />
             <Route path="/nurseform" element={<NurseForm />} />
+            <Route path="/queue" element={<Queue />} />
         </Routes>
     )
 }
