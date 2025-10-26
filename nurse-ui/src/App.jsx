a6743415da9de8f21aedc9369c7a035d91789984
mawerb <maorbarzilay8@gmail.com>
2025-10-26 02:52:32 -0700

    Implemented Nurse_Voice to frontend

--- nurse-ui/src/App.jsx
+++ nurse-ui/src/App.jsx
@@ -8,7 +8,6 @@ import VoiceNurse from './pages/voice_nurse'
 function App() {
     return (
         <Routes>
-            <Route path="/input_test" element={<InputLoginPage />} />
             <Route path="/voice_nurse" element={<VoiceNurse />} />
             <Route path="/" element={<Queue />} />
             <Route path="/inputtest" element={<InputLoginPage />} />
