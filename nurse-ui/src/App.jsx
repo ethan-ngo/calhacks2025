ea217f59618224d1299312d61937532afdae8838
ethan-ngo <ethanngo3539@gmail.com>
2025-10-25 20:03:55 -0700

    landing page

--- nurse-ui/src/App.jsx
+++ nurse-ui/src/App.jsx
@@ -5,6 +5,7 @@ import NurseForm from './pages/nurse_form'
 function App() {
     return (
         <Routes>
+            <Route path="/" element={<p>Hello World</p>} />
             <Route path="/inputtest" element={<InputLoginPage />} />
             <Route path="/nurseform" element={<NurseForm />} />
         </Routes>
