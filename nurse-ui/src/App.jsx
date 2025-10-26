5c544a3454edae5ef2c5b444b548be37128f1a45
mawerb <maorbarzilay8@gmail.com>
2025-10-25 23:03:47 -0700

    Implenting front-end

--- nurse-ui/src/App.jsx
+++ nurse-ui/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import InputLoginPage from './pages/input_test'
+import VoiceNurse from './pages/voice_nurse'
 import NurseForm from './pages/nurse_form'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <Routes>
             <Route path="/" element={<p>Hello World</p>} />
             <Route path="/inputtest" element={<InputLoginPage />} />
+            <Route path="/voice_nurse" element={<VoiceNurse />} />
             <Route path="/nurseform" element={<NurseForm />} />
         </Routes>
     )
